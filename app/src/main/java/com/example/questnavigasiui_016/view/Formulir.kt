b570c4a29523f8cbf789a0f0e92820a85cfbb87c
HafizZada16 <hafizzada16@gmail.com>
2025-11-07 15:06:36 +0700

    Membuat Scaffold

--- app/src/main/java/com/example/questnavigasiui_016/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_016/view/Formulir.kt
@@ -1,14 +1,33 @@
 package com.example.questnavigasiui_016.view
 
-import android.R
+
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_016.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    jenisK:List<R.string> = listOf("Laki-laki","Perempuan"),
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
     OnSumbitBtnClick: () -> Unit,
 ){
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(stringResource(id=R.string.home), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(id = R.color.teal_200))
+            ) }
+    ){ isiRuang ->
 
+
+    }
 }
