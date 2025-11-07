f169edbd69a0a9b5c3318156fe84579a322b8886
HafizZada16 <hafizzada16@gmail.com>
2025-11-07 16:07:30 +0700

    Membuat Scaffold TampilData

--- app/src/main/java/com/example/questnavigasiui_016/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_016/view/TampilData.kt
@@ -2,7 +2,14 @@ package com.example.questnavigasiui_016.view
 
 import com.example.questnavigasiui_016.R
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,4 +22,11 @@ fun TampilData(
         Pair(stringResource(R.string.jenis_kelamin), "Isinya"),
         Pair(stringResource(R.string.alamat), "Yogyakarta")
     )
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(stringResource(id = R.string.Tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(R.color.teal_200))
+            )
+        }){}
 }
