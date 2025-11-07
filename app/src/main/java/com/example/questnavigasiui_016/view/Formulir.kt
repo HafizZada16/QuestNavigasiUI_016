a895d012507b4f3f5cd6a859487a2445ef1fd17a
HafizZada16 <hafizzada16@gmail.com>
2025-11-07 15:35:57 +0700

    Membuat Button Submit

--- app/src/main/java/com/example/questnavigasiui_016/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_016/view/Formulir.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -81,7 +84,14 @@ fun FormIsian(
                 label = {Text(text = "Alamat")},
                 onValueChange = { },
             )
-            Spacer()
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(if)
+                    .padding(all = 20.dp),
+                onClick = OnSumbitBtnClick
+            ){
+
+                }
         }
 
     }
