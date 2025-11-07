80649c278f7d78255df6fb6a011b5df5f3b8e848
HafizZada16 <hafizzada16@gmail.com>
2025-11-07 16:22:50 +0700

    Membuat Button Back

--- app/src/main/java/com/example/questnavigasiui_016/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_016/view/TampilData.kt
@@ -58,7 +58,13 @@ fun TampilData(
                     }
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
-                
+                Spacer(modifier = Modifier.height(15.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick
+                ) {
+                    Text(text = stringResource(R.string.back))
+                }
             }
         }
     }
