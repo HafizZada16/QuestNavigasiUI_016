a149d3176cfe4770840dc3842efb1f0ae145c05c
HafizZada16 <hafizzada16@gmail.com>
2025-11-07 16:01:40 +0700

    Membuat fun TampilData

--- app/src/main/java/com/example/questnavigasiui_016/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_016/view/TampilData.kt
@@ -1,2 +1,18 @@
 package com.example.questnavigasiui_016.view
 
+import com.example.questnavigasiui_016.R
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick:()-> Unit
+){
+    val items = listOf(
+        Pair(stringResource(R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(R.string.jenis_kelamin), "Isinya"),
+        Pair(stringResource(R.string.alamat), "Yogyakarta")
+    )
+}
