17cf23cd42319f2aee3630aa862870340ce38eb7
HafizZada16 <hafizzada16@gmail.com>
2025-11-07 15:10:52 +0700

    Membuat Column isiRuang

--- app/src/main/java/com/example/questnavigasiui_016/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_016/view/Formulir.kt
@@ -1,16 +1,22 @@
 package com.example.questnavigasiui_016.view
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.questnavigasiui_016.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,7 +33,19 @@ fun FormIsian(
                     colorResource(id = R.color.teal_200))
             ) }
     ){ isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
 
+            )
+
+        }
 
     }
 }
